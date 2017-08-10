dd3b0f91823625aead6646095f7d928ae6b520fe
Pius Wong <pius@pioslabs.com>
2017-08-10 14:50:59 -0500

    Add users list to check

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   // get Twitch data
+  var users = ["adafruit", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
   // display users & status
 
