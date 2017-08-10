e9f59fd9544c94d43e8a6db079cb4bc48dd75137
Pius Wong <pius@pioslabs.com>
2017-08-10 14:57:30 -0500

    Get user data for one test user

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 
   // get Twitch data
-  var users = ["adafruit", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+  var users = ["adafruit", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "pioslabs"]
 
   // display users & status
+  // example: https://wind-bow.glitch.me/twitch-api/users/pioslabs
+  var apiUser = "freecodecamp";
+  $.getJSON('https://wind-bow.glitch.me/twitch-api/users/'+apiUser+'?callback=?', function(data) {
+    console.log(data);
+  });
+
 
   // display stream data for eligible streamers
+  // example: https://wind-bow.glitch.me/twitch-api/streams/shortyyguy
+
 });
