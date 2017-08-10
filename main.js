8b1d536ce34eb2c6c2a9bb9f31aba9b22e247d9d
Pius Wong <pius@pioslabs.com>
2017-08-10 15:32:21 -0500

    Add basic table for results

--- main.js
+++ main.js
@@ -15,8 +15,18 @@ $(document).ready(function(){
 });
 
 function getUserData(user){
+  // check if user actually exists
   $.getJSON('https://wind-bow.glitch.me/twitch-api/users/'+user+'?callback=?', function(data) {
-    console.log(data); // debug
-    $("#results").append(data.bio);// debug
+    //console.log(data); // debug
+    if(data.hasOwnProperty("error")){
+      console.log("User " + user + " not found.")//debug
+    }
+    else{
+      var output = '<tr class="row"><td class="user">%USER%</td><td class="bio">%BIO%</td></tr>';
+      output = output.replace("%USER%",data.display_name);
+      output = output.replace("%BIO%",data.bio);
+      $("#results table").append(output);// debug
+      console.log(output); // debug
+    }
   });
 }
