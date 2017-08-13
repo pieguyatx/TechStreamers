aed49c0bc30fa26fd663ead6d02316d6c1b98e03
Pius Wong <pius@pioslabs.com>
2017-08-12 20:37:52 -0500

    User names link to Twitch profiles

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ function getUserData(user){
       console.log("User " + user + " not found.")//debug
     }
     else{ // if user exists, display info
-      var output = '<div class="row userData" id="'+user+'"><div class="user">%USER%</div><div class="info"><div><div class="bio">%BIO%</div><div class="status"></div></div></div></div>';
-      output = output.replace("%USER%",data.display_name);
+      var output = '<div class="row userData" id="'+user+'"><div class="user"><a href="%LINKUSER%" target="_blank" title="Visit %USER%&#39;s profile (opens in new window)">%USER%</a></div><div class="info"><div><div class="bio">%BIO%</div><div class="status"></div></div></div></div>';
+      output = output.replace("%LINKUSER%","https://www.twitch.tv/"+user);
+      output = output.replace(/%USER%/g,data.display_name);
       output = output.replace("%BIO%",data.bio);
       $("#results .container").append(output);// debug
-      // console.log(output); // debug
       // get streaming info on existing user
       getStreamData(user);
     }
@@ -33,7 +33,8 @@ function getStreamData(user){
   $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/'+user+'?callback=?', function(data){
     // if stream detected, display data
     if(data.stream){
-      console.log(data["_links"].self); // debug
+      // console.log(data["_links"].self); // debug
+      // display stream status
       let output = "Streaming now!";
       $("#"+user+" .status").html(output).addClass("streaming");
       // display info about what's streaming
@@ -48,7 +49,4 @@ function getStreamData(user){
       $("#"+user+" .status").html(output).addClass("offline");
     }
   });
-
-  // display stream data for eligible streamers
-
 }
