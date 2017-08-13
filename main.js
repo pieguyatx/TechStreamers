889f1fa4dbae1701ff0922518d64b923417c9645
Pius Wong <pius@pioslabs.com>
2017-08-13 16:46:57 -0500

    Format header, adjust result colors

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // get Twitch data
-  var users = ["adafruit", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "noobs2ninjas", "pioslabs", "CapcomFighters"]
+  var users = ["adafruit", "ESL_SC2", "freecodecamp", "storbeck", "habathcx", "noobs2ninjas", "pioslabs"]
   // display users & status
   // example: https://wind-bow.glitch.me/twitch-api/users/pioslabs
   for(let i=0; i<users.length; i++){
